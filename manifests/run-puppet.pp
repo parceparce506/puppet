ef6708170990d96079d56b5f6b747203c85e0ab1
parceparce506 <52510385+parceparce506@users.noreply.github.com>
2019-07-03 15:29:41 -0600

    Fixed manifest to run on Debian

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,11 +1,11 @@
 # Set up regular Puppet runs
-file { '/usr/bin/run-puppet':
-  source => '/home/vagrant/puppet/files/run-puppet.sh',
+file { '/usr/local/bin/run-puppet':
+  source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
   mode   => '0755',
 }
 
 cron { 'run-puppet':
-  command => '/usr/bin/run-puppet',
+  command => '/usr/local/bin/run-puppet',
   hour    => '*',
   minute  => '*/15',
 }
